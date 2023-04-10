d9a0b6486fb40bb8768293a395925aecc8fe3ff3
Francisquet Vargas <fvs00725@gmail.com>
2023-04-09 21:49:18 -0500

    added data validation with pydantics and function to dynamically provide id for each book created

--- books.py
+++ books.py
@@ -1,5 +1,6 @@
+from typing import Optional
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -18,11 +19,11 @@ class Book:
         self.rating = rating
         
 class BookRequest(BaseModel):
-    id: int
-    title: str
-    author: str
-    description: str
-    rating: int
+    id: Optional[int]
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
     
 
 BOOKS = [
@@ -40,4 +41,8 @@ async def get_all_books():
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.dict())
-    BOOKS.append(new_book)
+    BOOKS.append(find_book_id(new_book))
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
