b2e211aace1c6fbbd2b74df204e14c09d04c0e9f
Francisquet Vargas <fvs00725@gmail.com>
2023-04-09 22:12:52 -0500

    added pydantic config class to customize the example schema on our Swagger UI

--- books.py
+++ books.py
@@ -19,11 +19,21 @@ class Book:
         self.rating = rating
         
 class BookRequest(BaseModel):
-    id: Optional[int]
+    id: Optional[int] = Field(title="id is not needed")
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "A new book",
+                "author": "Frank Vargas",
+                "description": "A new description of a book",
+                "rating": 5
+            }
+        }
     
 
 BOOKS = [
